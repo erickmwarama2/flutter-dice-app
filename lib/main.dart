7fb0b6fea8baa4a4e440ee685dcdb6769c7a8028
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-03-26 10:34:06 +0300

    create custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,29 @@ void main() {
     MaterialApp(
       home: Scaffold(
         // backgroundColor: Color(0xff6ca439),
-        body: Container(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(colors: [
               Color(0xff6ca439),
               Colors.yellowAccent,
               Color.fromARGB(255, 45, 18, 134)
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
-            ),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
           ),
           child: const Center(
-            child: Text('Hello World from Erick',
-            style: TextStyle(
-              color: Colors.redAccent,
-              fontSize: 30
-              ),),
+            child: Text(
+              'Hello World from Erick',
+              style: TextStyle(color: Colors.redAccent, fontSize: 30),
+            ),
           ),
-        ),
-      ),
-    ),
-  );
+        );
+  }
 }
