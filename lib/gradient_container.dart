66207cd09c073c2e4c2636fe7ef4fa37720fcdd1
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-03-27 10:36:26 +0300

    add stateful widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,16 +1,15 @@
+import 'package:dice/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
+// ignore: must_be_immutable
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
 
   final List<Color> colors;
 
-  void rollDice() {
-
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,20 +18,8 @@ class GradientContainer extends StatelessWidget {
         gradient: LinearGradient(
             colors: colors, begin: startAlignment, end: endAlignment),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-2.png',
-              width: 200,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
